4f952d61e4dc4ee4fd8dfd98999ef3513d29e799
APPLEH4CKZ <42897517+APPLEH4CKZ@users.noreply.github.com>
2019-03-27 16:48:16 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function clicked() {
+
+	var user = document.getElementById('username');
+	var pass = document.getElementById('password');
+	
+	var people = [
+	{
+		username: "sam",
+		password: "codify"
+	},
+	{
+		username: "matt",
+		password: "academy"
+	},
+	{
+		username: "chris",
+		password: "forever"
+	}
+
+	if(user == people) {
+	
+		if(password == people) {
+		
+			window.alert("You are logged in as " + user.value);
+			window.open("http://www.nicksdesk.com");
+		
+		} else {
+		
+			window.alert("Incorrect username or password!");
+		
+		}
+	
+	} else {
+	
+			window.alert("Incorrect username or password!");
+	
+	}
+
+}
